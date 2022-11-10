b804df4b5024bae1340bcee49260cfddf2611e0c
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-11-10 11:23:19 -0500

    exercise complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var wordReversed = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                wordReversed += word[i];
+            }
+            return wordReversed == word;
+        }
+    }
+   
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
-using System;
+﻿using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)] // ← you will need to put some test data and expected result here.  
+        [InlineData("trackcar", false)]
+        // For example [InlineData(“racecar”, true)]
+        public void Test1(string word, bool expected)
         {
-
+            //Arrange (instantiate an object of class WordSmith)
+            var iCheckForPalindromes = new WordSmith();
+            //Act 
+            var actual = iCheckForPalindromes.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(actual, expected);
         }
     }
 }
